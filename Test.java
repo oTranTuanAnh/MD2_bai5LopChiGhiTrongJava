11cc312e057996786a57bfc76b650081cdc9a6c9
tuan anh <tuananhtran.xd@gmail.com>
2023-12-14 17:15:37 +0700

    first commit

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+        Student st = new Student();
+        st.setName("abc");
+        st.setClasses("10");
+        System.out.println(st);
+    }
+
+
+}
